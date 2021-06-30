755ae0349cb54c1638783f52ba105d1c1f4b6c74
Stepan Savinov <stepa.savinoff@gmail.com>
2021-06-30 11:12:16 +0400

    Installed webpack and react

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+    return (
+        <div className="container">
+            <div className="app">
+                Hello, world!
+            </div>
+        </div>
+    );
+};
+
+export default App;
